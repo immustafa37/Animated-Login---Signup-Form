bbe494b65651e95a25e083d80436801eb209a90e
iM Mustafa <im262897tt@gmail.com>
2025-02-20 21:49:20 -0800

    start javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle between Sign In and Sign Up
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
